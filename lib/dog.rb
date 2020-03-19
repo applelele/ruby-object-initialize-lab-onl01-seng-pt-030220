28b9d7d7201f18f5ff130f416132d52a06cc3307
YUKIKO IKEDA <applelele@gmail.com>
2020-03-19 00:00:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,12 @@
 class Dog
   def initialize(name, breed)
     @name = name
+    @breed = breed
+  end
+  
+  def breed
     if breed.length > 0
-      @breed = breed
+      @breed
     else
       breed = "Mutt"
     end
